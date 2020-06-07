8f9536c90ca0fe0fc9ebe5f1deac27ea9dda76c7
Jarret Berman <jarretberman11@gmail.com>
2020-06-06 19:01:47 -0500

    mvp commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template, redirect, session
+# from flask_debugtoolbar import DebugToolbarExtension
+from surveys import Survey, Question, surveys
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "guitar"
+
+# debug = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def start_page():
+    return render_template('start.html', survey = surveys['satisfaction'])
+
+@app.route('/session', methods=['POST'])
+def set_session():
+    session['responses'] = []
+    return redirect('/questions/0')
+
+@app.route('/questions/<int:num>')
+def question_page(num):
+    responses = session['responses']
+    #redirect to proper sequential question
+    if num > len(responses) or num < len(responses):
+        return redirect(f'/questions/{len(responses)}')
+
+    #remove access to questions after completion
+    if len(responses) == len(surveys['satisfaction'].questions) :  
+        return redirect('/thanks')
+
+    return render_template('question.html', num = num, question = surveys['satisfaction'].questions[num-1])
+
+@app.route('/answer', methods = ["POST"])
+def handle_answer():
+    answer = request.form['answer']
+    
+    responses = session['responses']
+    responses.append(answer)
+    session['responses'] = responses
+    return redirect(f'/questions/{len(responses)}')
+
+@app.route('/thanks')
+def thankyou_page():
+    return render_template('thanks.html')
